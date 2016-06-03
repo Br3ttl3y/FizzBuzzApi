689a1db73032eb8973fc6c60d3071f947309a4ea
Brett Carroll <brett.carroll@gmail.com>
2016-06-03 15:10:46 -0500

    Initial Check In
    Created a fizzbuzz implementation that compiles to a DLL and has a Test
    Project.

---
+++ FizzBuzzTests/FizzBuzzUnitTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using FizzBuzzApi;
+
+namespace FizzBuzzTests
+{
+    [TestClass]
+    public class FizzBuzzUnitTests
+    {
+        [TestMethod]
+        public void TestClassic()
+        {
+            // Act
+            List<string> results = FizzBuzz.Classic();
+
+            // Assert
+            Assert.AreEqual("3: Fizz", results[2]);
+            Assert.AreEqual("5: Buzz", results[4]);
+            Assert.AreEqual("15: FizzBuzz", results[14]);
+        }
+
+        [TestMethod]
+        public void TestCustomStartAndEnd()
+        {
+            // Arrange
+            int start = 1;
+            int end = 15;
+
+            // Act
+            List<string> results = FizzBuzz.CustomStartAndEnd(start, end);
+
+            // Assert
+            Assert.AreEqual("3: Fizz", results[2]);
+            Assert.AreEqual("5: Buzz", results[4]);
+            Assert.AreEqual("15: FizzBuzz", results[14]);
+        }
+
+        [TestMethod]
+        public void TestCustomWithClassicRules()
+        {
+            // Arrange
+            FizzBuzzConfig config = new FizzBuzzConfig
+            {
+                Start = 1,
+                End = 100,
+                Words = new Dictionary<int, string> { { 3, "Fizz"}, { 5, "Buzz"} }
+            };
+
+            // Act
+            List<string> results = FizzBuzz.CustomConfig(config);
+
+            // Assert
+            Assert.AreEqual("3: Fizz", results[2]);
+            Assert.AreEqual("5: Buzz", results[4]);
+            Assert.AreEqual("15: FizzBuzz", results[14]);
+        }
+
+        [TestMethod]
+        public void TestCustomWithCustomGame()
+        {
+            // Arrange
+            FizzBuzzConfig config = new FizzBuzzConfig
+            {
+                Start = 1,
+                End = 20,
+                Words = new Dictionary<int, string> { { 2, "Fizz"}, { 3, "Buzz" }, { 4, "Bazz" } }
+            };
+
+            // Act
+            List<string> results = FizzBuzz.CustomConfig(config);
+
+            // Assert
+            Assert.AreEqual("2: Fizz", results[1]);
+            Assert.AreEqual("3: Buzz", results[2]);
+            Assert.AreEqual("4: FizzBazz", results[3]);
+            Assert.AreEqual("6: FizzBuzz", results[5]);
+            Assert.AreEqual("12: FizzBuzzBazz", results[11]);
+        }
+    }
+}
---
+++ NewConsoleApplication/FizzBuzz.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzApi
+{
+    public static class FizzBuzz
+    {
+        public static List<string> Classic()
+        {
+            return CustomStartAndEnd(1, 100);
+        }
+
+        public static List<string> CustomStartAndEnd(int start, int end)
+        {
+            List<string> results = new List<string>();
+
+            for (int i = start; i <= end; i++)
+            {
+                if (i % 3 != 0 && i % 5 != 0)
+                {
+                    results.Add(i.ToString());
+                }
+
+                if (i % 3 == 0 && i % 5 == 0)
+                {
+                    results.Add(i + ": FizzBuzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    results.Add(i + ": Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    results.Add(i + ": Buzz");
+                }
+            }
+
+            return results;
+        }
+
+        public static List<string> CustomConfig(FizzBuzzConfig gameConfig)
+        {
+            List<string> results = new List<string>();
+
+            for (int i = gameConfig.Start; i <= gameConfig.End; i++)
+            {
+                bool hasAtLeastOneFactor = false;
+                string gameResult = i + ": ";
+
+                foreach (int factor in gameConfig.Words.Keys)
+                {
+                    if (i % factor != 0)
+                    {
+                        continue;
+                    }
+
+                    hasAtLeastOneFactor = true;
+
+                    gameResult += gameConfig.Words[factor];
+                }
+
+                if (hasAtLeastOneFactor == false)
+                {
+                    results.Add(i.ToString());
+                }
+                else
+                {
+                    results.Add(gameResult);
+                }
+            }
+
+            return results;
+        }
+    }
+}
